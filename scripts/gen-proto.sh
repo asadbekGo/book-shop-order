b282d442a59fccb651b81b7f50f6ee4a1046b6cd
asadbek <asadbek30665@gmail.com>
2022-01-11 10:19:26 +0500

    order service call catalog service

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -2,6 +2,14 @@
 
 CURRENT_DIR=$(pwd)
 
+mkdir $CURRENT_DIR/genproto/catalog_service
+mkdir $CURRENT_DIR/genproto/order_service
+
+protoc -I /usr/local/include \
+       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $CURRENT_DIR/protos/catalog_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/catalog_service/ \
+        $CURRENT_DIR/protos/catalog_service/*.proto;
 
 protoc -I /usr/local/include \
        -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
